42cd2574247a59cc63f42c2803e76fc9775fbe85
Joseph Hunkeler <jhunkeler@gmail.com>
2016-02-28 22:44:24 -0500

    Stop showing literal boolean value in UI

--- src/ui/flag.filter.js
+++ src/ui/flag.filter.js
@@ -1,5 +1,10 @@
 app.filter('flag', function() {
     return function(input) {
+        if (input) {
+            input = 1;
+        } else {
+            input = 0;
+        }
         return input.toString().toUpperCase();
     };
 });
