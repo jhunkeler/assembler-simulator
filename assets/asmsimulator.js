9a47ffd020c31ef54e52e383f5d168c811e26664
Joseph Hunkeler <jhunkeler@gmail.com>
2016-02-29 11:21:47 -0500

    Make sample code easier to edit

--- assets/asmsimulator.js
+++ assets/asmsimulator.js
@@ -1403,7 +1403,36 @@ var app = angular.module('ASMSimulator', []);
     $scope.speed = 16;
     $scope.outputStartIndex = 436;
 
-    $scope.code = "; Simple example\n; Writes Hello World to the output\n\n	JMP start\nhello: DB \"Hello World!\" ; Variable\n       DB 0	; String terminator\n\nstart:\n	MOV C, hello    ; Point to var \n	MOV D, 232	; Point to output\n	CALL print\n        HLT             ; Stop execution\n\nprint:			; print(C:*from, D:*to)\n	PUSH A\n	PUSH B\n	MOV B, 0\n.loop:\n	MOV A, [C]	; Get char from var\n	MOV [D], A	; Write to output\n	INC C\n	INC D  \n	CMP B, [C]	; Check if end\n	JNZ .loop	; jump if not\n\n	POP B\n	POP A\n	RET";
+    $scope.code = "" +
+"; Simple example\n" + 
+"; Writes Hello World to the output\n" +
+"\n" +
+"JMP start\n" +
+"hello: DB \"Hello World!\" ; Variable\n" +
+"       DB 0        ; String terminator\n" +
+"\n" +
+"start:\n" +
+"	MOV C, hello    ; Point to var\n" + 
+"	MOV D, 436      ; Point to output\n" +
+"	CALL print\n" +
+"        HLT           ; Stop execution\n" +
+"\n" +
+"print:                ; print(C:*from, D:*to)\n" +
+"	PUSH A\n" +
+"	PUSH B\n" +
+"	MOV B, 0\n" +
+".loop:\n" +
+"	MOV A, [C]    ; Get char from var\n" +
+"	MOV [D], A    ; Write to output\n" +
+"	INC C\n" +
+"	INC D\n" +
+"	CMP B, [C]    ; Check if end\n" +
+"	JNZ .loop     ; jump if not\n" +
+"\n" +
+"	POP B\n" +
+"	POP A\n" +
+"	RET\n" +
+"\n";
 
     $scope.reset = function () {
         cpu.reset();
